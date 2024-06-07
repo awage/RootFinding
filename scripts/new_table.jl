35abbf4b743698157bc1041240c7676b7012fd0d
awage <>
2024-06-07 15:24:56 +0200

    format table for latex

--- scripts/new_table.jl
+++ scripts/new_table.jl
@@ -22,7 +22,12 @@ function print_table_all()
     ax = Axis(ff[1,1], 
     ylabel = L"\eta", xlabel = L"\beta", yticklabelsize = 30, xticklabelsize = 30, ylabelsize = 40, xlabelsize = 40,  titlesize = 30)
     for i in [1:13; 15]
-        println("|funct: ", i, " |  IT  | EX μs| NC %  | OR |")
+        println("Function f",i, "=", string_list[i] )
+        println(" ")
+        println("\\begin{tabular}{l|l|l|l|l|l|l|l|l}")
+        println("\\hline\\")
+        println(L" $\beta$ &  OR & IT  & NC (\%) & T ($\mu$ s) & P/s ($\times 10^6$) & IT/s  ($\times 10^6$) & Sb & Sbb \\\\")
+        println("\\hline\\")
         for (k,β) in enumerate(β_range)
             data0 = _get_basins(func_list[i], β, i, res, ε, max_it)
             @unpack q, attractors,iterations,basins, Sb, Sbb, exec_time = data0
@@ -34,16 +39,20 @@ function print_table_all()
             ps = length(ind)/sum(exec_time[ind])
             its = sum(iterations[ind])/sum(exec_time[ind])
             
-            println("| ", "β = ", β, 
-                " | ", round(q, digits =2),
-                " | ", round(mit, digits =2), 
-                " | ", round(nc*100, digits =2),
-                " | ", round(t0*1e6, digits =2), 
-                " | ", round(ps*1e-6, digits =2),
-                " | ", round(its*1e-6, digits =2),
-                " | ", round(Sb, digits =2),
-                " | ", round(Sbb, digits =2))
+            println(β, 
+                " & ", round(q, digits =2),
+                " & ", round(mit, digits =2), 
+                " & ", round(nc*100, digits =2),
+                " & ", round(t0*1e6, digits =2), 
+                " & ", round(ps*1e-6, digits =2),
+                " & ", round(its*1e-6, digits =2),
+                " & ", round(Sb, digits =2),
+                " & ", round(Sbb, digits =2), "\\\\")
+
         end
+        println("\\hline")
+        println("\\end{tabular}")
+        println(" ")
     end
 end
 print_table_all()
