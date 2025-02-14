8b128289d9e9e0e1a9551e1aa7dae0e88c8557b4
alex <mac@macs-MacBook-Pro.local>
2025-02-14 08:49:36 +0100

    figs

--- src/function_stuff.jl
+++ src/function_stuff.jl
@@ -105,6 +105,9 @@ function _stephenson_map(f::Function, g::Function)
         fx = f(x) 
         gx = g(fx) 
         fx_h = f(x + gx)
+        @show fx, gx, fx_h
+        @show  fx*gx/(fx_h - fx)
+
         x_new = x - fx*gx/(fx_h - fx)
         return x_new, fx
     end
